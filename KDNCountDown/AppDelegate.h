3bd2a4f9a7ef21ca3d8c41dca9be969d4ceb94f4
kaden <kaden@yahoo-inc.com>
2015-06-07 10:00:11 +0800

    Initial Commit

---
+++ KDNCountDown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KDNCountDown
+//
+//  Created by kaden Chiang on 2015/6/7.
+//  Copyright (c) 2015年 kaden Chiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KDNCountDown/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KDNCountDown
+//
+//  Created by kaden Chiang on 2015/6/7.
+//  Copyright (c) 2015年 kaden Chiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
